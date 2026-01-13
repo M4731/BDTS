52a96dbd59af8811abf450dfc615bdb59778d473
Mîndruleanu Matei Daniel <mateimindruleanu@gmail.com>
2026-01-13 14:04:36 +0200

    asi project backend
    spring new project + structure

---
+++ Analiza Unui Sistem Informatic/demo/src/main/java/com/proiectasi/demo/ProiectasiApplication.java
@@ -0,0 +1,13 @@
+package com.proiectasi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectasiApplication.class, args);
+	}
+
+}
---
+++ Analiza Unui Sistem Informatic/demo/src/test/java/com/proiectasi/demo/ProiectasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proiectasi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
