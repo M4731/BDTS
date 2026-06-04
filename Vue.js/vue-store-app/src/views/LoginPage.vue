2f1ac28f72fa3d423a07b06574d2fab342407b8c
Mîndruleanu Matei Daniel <mateimindruleanu@gmail.com>
2026-06-04 12:41:58 +0300

    vue din curs

---
+++ Vue.js/vue-store-app/src/views/LoginPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Login</h1>
+
+    <input
+      v-model="email"
+      placeholder="Email"
+    >
+
+    <input
+      v-model="password"
+      type="password"
+      placeholder="Parola"
+    >
+
+    <button @click="login">
+      Login
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+
+  data() {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+
+  methods: {
+    login() {
+      alert("Login apăsat")
+    }
+  }
+}
+</script>
